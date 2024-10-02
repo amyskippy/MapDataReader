ab4f9279969da0ceff28ba5c86bc9644d1a6f28a
Amy <amyskippy@outlook.com>
2024-10-02 15:59:19 +0100

    feat: added AccessModifier property to the GenerateDataReaderMapper attribute

--- MapDataReader/MapperGenerator.cs
+++ MapDataReader/MapperGenerator.cs
@@ -11,6 +11,17 @@ namespace MapDataReader
 	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 	public class GenerateDataReaderMapperAttribute : Attribute
 	{
+		public string AccessModifier { get; set; }
+
+		public GenerateDataReaderMapperAttribute()
+		{
+			AccessModifier = "public";
+		}
+
+		public GenerateDataReaderMapperAttribute(string access = "public")
+		{
+			AccessModifier = access;
+		}
 	}
 
 	[Generator]
