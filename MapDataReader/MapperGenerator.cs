2761375c99518f27caed941ab16aeea605d941d0
Amy <amyskippy@outlook.com>
2024-10-05 14:01:33 +0100

    feat: checks the underlying value of enums for casting boxed value to enum directly
    This now uses the same code as the primitive casting and preserves the underlying type of the enum

--- MapDataReader/MapperGenerator.cs
+++ MapDataReader/MapperGenerator.cs
@@ -55,16 +55,23 @@ namespace MapDataReader
 									{
 										return $@"	if (name == ""{p.Name.ToUpperInvariant()}"") {{ target.{p.Name} = value as {pTypeName}; return; }}";
 									}
-									else if (pTypeName.EndsWith("?") && !p.Type.IsNullableEnum()) //nullable type (unless nullable Enum)
+
+									if (p.Type.TryGetNullableValueUnderlyingType(out var underlyingType) && !underlyingType.IsEnum()) //nullable type (unless nullable Enum)
 									{
 										var nonNullableTypeName = pTypeName.TrimEnd('?');
 
 										//do not use "as" operator becasue "as" is slow for nullable types. Use "is" and a null-check
 										return $@"	if (name == ""{p.Name.ToUpperInvariant()}"") {{ if(value==null) target.{p.Name}=null; else if(value is {nonNullableTypeName}) target.{p.Name}=({nonNullableTypeName})value; return; }}";
 									}
-									else if (p.Type.TypeKind == TypeKind.Enum || p.Type.IsNullableEnum()) //enum? pre-convert to underlying type then to int, you can't cast a boxed int to enum directly. Also to support assigning "smallint" database col to int32 (for example), which does not work at first (you can't cast a boxed "byte" to "int")
+
+									if (p.Type.TryGetEnum(out var enumTypeSymbol) || underlyingType.TryGetEnum(out enumTypeSymbol))
 									{
-										return $@"	if (value != null && name == ""{p.Name.ToUpperInvariant()}"") {{ target.{p.Name} = ({pTypeName})(value.GetType() == typeof(int) ? (int)value : (int)Convert.ChangeType(value, typeof(int))); return; }}"; //pre-convert enums to int first (after unboxing, see below)
+										var eTypeName = enumTypeSymbol.FullName(); // e.g. int, byte, short
+										
+										// enum? pre-convert to underlying type then to int, you can't cast a boxed int to enum directly.
+										// Also to support assigning "smallint" database col to int32 (for example), which does not work at first (you can't cast a boxed "byte" to "int")
+										// pre-convert enums to eTypeName (int, byte, short) first (after unboxing, see below)
+										return $@"	if (value != null && name == ""{p.Name.ToUpperInvariant()}"") {{ target.{p.Name} = ({pTypeName})(value.GetType() == typeof({eTypeName}) ? ({eTypeName})value : ({eTypeName})Convert.ChangeType(value, typeof({eTypeName}))); return; }}"; 
 									}
 									else //primitive types. use Convert.ChangeType before casting. To support assigning "smallint" database col to int32 (for example), which does not work at first (you can't cast a boxed "byte" to "int")
 									{
